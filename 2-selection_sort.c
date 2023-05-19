baaa181c752188a01a933045720d294c2538c11e
root <lautarorodriguezcoitinho@gmail.com>
2023-05-19 13:52:41 -0700

    Task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+void
+swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void
+selection_sort(int *array, size_t size)
+{
+		size_t iterator, min_i, step;
+
+		for (step = 0; step < size - 1; step++)
+		{
+			min_i = step;
+			for (iterator = step + 1; iterator < size; iterator++)
+			{
+				if (array[iterator] < array[min_i])
+					min_i = iterator;
+			}
+			swap(&array[min_i], &array[step]);
+			print_array(array, size);
+		}
+
+
+}
