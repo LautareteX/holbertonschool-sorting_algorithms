ca00cd0b060a3fa382ad3cbb4dafa0d075828264
root <lautarorodriguezcoitinho@gmail.com>
2023-05-19 14:06:55 -0700

    All identicly

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,16 +15,24 @@ selection_sort(int *array, size_t size)
 
 		if (array)
 		{
-		for (step = 0; step <= size - 1; step++)
+		for (step = 0; step < size - 1; step++)
 		{
 			min_i = step;
-			for (iterator = step + 1; iterator <= size; iterator++)
+			for (iterator = step + 1; iterator < size; iterator++)
 			{
-				if (array[iterator] <= array[min_i])
+				if (array[iterator] < array[min_i])
 					min_i = iterator;
 			}
+			if (array[min_i] != array[step])
+			{
 			swap(&array[min_i], &array[step]);
 			print_array(array, size);
+			}
+			else
+			{
+				print_array(array, size);
+				exit(0);
+			}
 		}
 		}
 		else
@@ -32,3 +40,4 @@ selection_sort(int *array, size_t size)
 			exit(0);
 		}
 }
+
